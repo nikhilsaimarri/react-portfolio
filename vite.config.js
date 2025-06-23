2d394c78b7c45a8da207cf223814c3c8748cd50e
nikhilsaimarri <nikhilmarri9872@gmail.com>
2025-06-22 23:05:25 -0500

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-portfolio/',  // 👈 Add this line
   plugins: [react()],
 })
