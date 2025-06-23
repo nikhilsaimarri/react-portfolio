6b0c7a84f5c30ac38ed846821ea9f781f39280f7
nikhilsaimarri <nikhilmarri9872@gmail.com>
2025-06-22 23:28:51 -0500

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/react-portfolio/',  // 👈 Add this line
-  plugins: [react()],
-})
